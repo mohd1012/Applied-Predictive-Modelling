6df77c3d7a05a0c482711c975565b234292745b1
Matthew11235 <matthew.lay@riotinto.com>
2015-07-19 14:48:49 +1000

    Added heatmap code for variable contribution Did lots of tidying up building to more of a function call for running all models

--- ML_on_diamonds.R
+++ ML_on_diamonds.R
@@ -26,6 +26,7 @@ library(parallel)
 library(gbm)
 library(Cubist)
 library(party)
+library(tidyr)
 
 # create dataset and base model
 # scriptLocation()
@@ -48,72 +49,63 @@ inTrain <- createDataPartition(data_set$price, p = .8)[[1]]
 data_set_train <- data_set[ inTrain, ]
 data_set_test  <- data_set[-inTrain, ]
 
-# Fit linear model
-set.seed(1)
-linear_model <- train(formula, 
-                data = data_set_train,
-                method = "lm", 
-                trControl = trainControl(method = "cv"))
-linear_model
+# Setup common caret parameters
+training_control <- trainControl(method = "cv")
 
-testResults <- data.frame(obs = data_set_train[,1],
-                          Linear_Regression = predict(linear_model))
-plot(testResults)
+
+# Fit linear model
+set.seed(100)
+linear_model <- train(formula, 
+                      data = data_set_train,
+                      method = "lm", 
+                      trControl = training_control)
 
 # Fit second order linear model
 formula_poly <- price ~ poly(carat, depth, table, x, y, z, degree = 2)
 
+set.seed(100)
 polynomial_model <- train(formula_poly, 
-                data = data_set_train,
-                method = "lm", 
-                trControl = trainControl(method = "cv"))
-polynomial_model
+                          data = data_set_train,
+                          method = "lm", 
+                          trControl = training_control)
 
 # Fit MARS model (Multivariate Adaptive Regression)
-set.seed(1)
+set.seed(100)
 mars_model <- train(formula, 
-                 data = data_set_train,
-                 method = "earth",
-                 tuneGrid = expand.grid(degree = 1, nprune = 2:38),
-                 trControl = trainControl(method = "cv"))
-mars_model
+                    data = data_set_train,
+                    method = "earth",
+                    tuneGrid = expand.grid(degree = 1, nprune = 2:38),
+                    trControl = training_control)
 
 # Set up SVM tunning grid
-set.seed(231)
+set.seed(100)
 sigDist <- sigest(price ~ ., data = data_set_train, frac = 1)
 svmTuneGrid <- data.frame(sigma = as.vector(sigDist)[1], C = 2 ^ (-2:7))
 
 # Fit SVM model
 # classProbs = TRUE was added since the text was written
-set.seed(1056)
+set.seed(100)
 svm_model <- train(price ~ .,
-                data = data_set_train,
-                method = "svmRadial",
-                preProc = c("center", "scale"),
-                tuneGrid = svmTuneGrid,
-                trControl = trainControl(method = "cv"))
-svm_model
-
-# A line plot of the average performance. The 'scales' argument is actually an 
-# argument to xyplot that converts the x-axis to log-2 units.
-plot(svm_model, scales = list(x = list(log = 2)))
+                   data = data_set_train,
+                   method = "svmRadial",
+                   preProc = c("center", "scale"),
+                   tuneGrid = svmTuneGrid,
+                   trControl = training_control)
 
 # Fit glm
-set.seed(1056)
+set.seed(100)
 glm_model <- train(price ~ .,
-                    data = data_set_train,
-                    method = "glm",
-                    trControl = trainControl(method = "cv"))
-glm_model
+                   data = data_set_train,
+                   method = "glm",
+                   trControl = training_control)
 
 # Fit partial least squares
 set.seed(100)
 tune_grid <- expand.grid(ncomp = 1:(ncol(data_set_train) - 2))
 pls_model <- train(x = data_set[, -1], y = data_set[, 1],
-                 method = "pls",
-                 tuneGrid = tune_grid,
-                 trControl = trainControl(method = "cv"))
-pls_model
+                   method = "pls",
+                   tuneGrid = tune_grid,
+                   trControl = training_control)
 
 # Fit principle components regression
 tune_grid <- expand.grid(ncomp = 1:min(ncol(data_set_train) - 2, 35))
@@ -121,29 +113,28 @@ pcr_model <- train(formula,
                    data = data_set_train,
                    method = "pcr",
                    tuneGrid = tune_grid,
-                   trControl = trainControl(method = "cv"))
-pcr_model
+                   trControl = training_control)
 
 # Fit ridge regression
 ridgeGrid <- expand.grid(lambda = seq(0, .1, length = 15))
+set.seed(100)
 ridge_model <- train(formula, 
-                    data = data_set_train,
-                    method = "ridge",
-                    tuneGrid = ridgeGrid,
-                    trControl = trainControl(method = "cv"),
-                    preProc = c("center", "scale"))
-ridge_model
+                     data = data_set_train,
+                     method = "ridge",
+                     tuneGrid = ridgeGrid,
+                     trControl = training_control,
+                     preProc = c("center", "scale"))
 
 # Fit enet model
 enetGrid <- expand.grid(lambda = c(0, 0.01, .1), 
                         fraction = seq(.05, 1, length = 20))
+set.seed(100)
 enet_model <- train(formula, 
                     data = data_set_train,
                     method = "enet",
                     tuneGrid = enetGrid,
-                    trControl = trainControl(method = "cv"),
+                    trControl = training_control,
                     preProc = c("center", "scale"))
-enet_model
 
 # Fit neural net
 nnetGrid <- expand.grid(decay = c(0, 0.01, .1), 
@@ -151,80 +142,61 @@ nnetGrid <- expand.grid(decay = c(0, 0.01, .1),
                         bag = FALSE)
 
 set.seed(100)
-
 nnet_model <- train(formula, 
-                  data = data_set_train,
-                  method = "avNNet",
-                  tuneGrid = nnetGrid,
-                  trControl = trainControl(method = "cv"),
-                  preProc = c("center", "scale"),
-                  linout = TRUE,
-                  trace = FALSE,
-                  MaxNWts = 13 * (ncol(data_set_train[,-1]) + 1) + 13 + 1,
-                  maxit = 1000,
-                  allowParallel = FALSE)
-nnet_model
-
-plot(nnet_model)
+                    data = data_set_train,
+                    method = "avNNet",
+                    tuneGrid = nnetGrid,
+                    trControl = training_control,
+                    preProc = c("center", "scale"),
+                    linout = TRUE,
+                    trace = FALSE,
+                    MaxNWts = 13 * (ncol(data_set_train[,-1]) + 1) + 13 + 1,
+                    maxit = 1000,
+                    allowParallel = FALSE)
 
 # knnet
+set.seed(100)
 knn_model <- train(formula, 
-                  data = data_set_train,
-                  method = "knn",
-                  preProc = c("center", "scale"),
-                  tuneGrid = data.frame(k = 1:20),
-                  trControl = trainControl(method = "cv"))
-
-knn_model
-
-plot(knn_model)
+                   data = data_set_train,
+                   method = "knn",
+                   preProc = c("center", "scale"),
+                   tuneGrid = data.frame(k = 1:20),
+                   trControl = training_control)
 
 # make rpart model
+set.seed(100)
 rpart_model <- train(formula, 
-                    data = data_set_train,
-                    method = "rpart",
-                    tuneLength = 25,
-                    trControl = trainControl(method = "cv"))
+                     data = data_set_train,
+                     method = "rpart",
+                     tuneLength = 25,
+                     trControl = training_control)
 
-plot(rpart_model, scales = list(x = list(log = 10)))
-
-rpart_imp <- varImp(rpart_model, scale = FALSE, competes = FALSE)
-rpart_imp
-
-rpart_tree <- as.party(rpart_model$finalModel)
-plot(rpart_tree)
 
 # Make conditional inference tree
 cGrid <- data.frame(mincriterion = sort(c(.95, seq(.75, .99, length = 2))))
+set.seed(100)
 ctree_model <- train(formula, 
-                    data = data_set_train,
-                    method = "ctree",
-                    tuneGrid = cGrid,
-                    trControl = trainControl(method = "cv"))
-ctree_model
-plot(ctree_model$finalModel)
+                     data = data_set_train,
+                     method = "ctree",
+                     tuneGrid = cGrid,
+                     trControl = training_control)
 
 # Make m5 model
+set.seed(100)
 m5_model <- train(formula, 
                   data = data_set_train,
                   method = "M5",
-                  trControl = trainControl(method = "cv"),
+                  trControl = training_control,
                   control = Weka_control(M = 10))
-m5_model
 
-plot(m5_model)
-
-rule_fit <- M5Rules(formula, data = data_set_train, control = Weka_control(M = 10))
-rule_fit
 
 # Make bagged tree model
+set.seed(100)
 treebag_model <- train(formula, 
-                      data = data_set_train,
-                      method = "treebag",
-                      nbagg = 50,
-                      trControl = trainControl(method = "cv"))
-
-treebag_model
+                       data = data_set_train,
+                       method = "treebag",
+                       nbagg = 50,
+                       trControl = training_control)
 
 # Make random forests model
 mtryGrid <- data.frame(mtry = floor(seq(10, ncol(data_set_train) - 1, length = 10)))
@@ -236,13 +208,7 @@ rf_model <- train(formula,
                   tuneGrid = mtryGrid,
                   ntree = 1000,
                   importance = TRUE,
-                  trControl = trainControl(method = "cv"))
-rf_model
-plot(rf_model)
-
-rfImp <- varImp(rf_model, scale = FALSE)
-rfImp
-
+                  trControl = training_control)
 
 # Boosted Trees
 # Added n.minobsinnode = 10 to expand grid. Seems this is now required, and 10 is a good default
@@ -259,8 +225,7 @@ gbm_model <- train(formula,
                    method = "gbm",
                    tuneGrid = mtryGrid,
                    verbose = FALSE,
-                   trControl = trainControl(method = "cv"))
-gbm_model
+                   trControl = training_control)
 
 # Cubist
 cbGrid <- expand.grid(committees = c(1:10, 20, 50, 75, 100), 
@@ -268,16 +233,10 @@ cbGrid <- expand.grid(committees = c(1:10, 20, 50, 75, 100),
 
 set.seed(100)
 cubist_model <- train(formula, 
-                    data = data_set_train,
-                    method = "cubist",
-                    tuneGrid = cbGrid,
-                    trControl = trainControl(method = "cv"))
-cubist_model
-
-plot(cubist_model, auto.key = list(columns = 4, lines = TRUE))
-
-cbImp <- varImp(cubist_model, scale = FALSE)
-cbImp
+                      data = data_set_train,
+                      method = "cubist",
+                      tuneGrid = cbGrid,
+                      trControl = training_control)
 
 
 # make model comparisons
@@ -293,7 +252,7 @@ resamp <- resamples(model_list)
 summary(resamp)
 parallelplot(resamp, metric = "Rsquared")
 
-# list of models setup. Couldn't get gbm working
+# list of models setup
 linear_model
 polynomial_model
 mars_model
@@ -313,13 +272,27 @@ gbm_model
 rf_model
 cubist_model
 
+# Some customised model plots:
+plot(rpart_model, scales = list(x = list(log = 10)))
+# A line plot of the average performance. The 'scales' argument is actually an 
+# argument to xyplot that converts the x-axis to log-2 units.
+plot(svm_model, scales = list(x = list(log = 2)))
+rpart_tree <- as.party(rpart_model$finalModel)
+plot(rpart_tree)
+plot(ctree_model$finalModel)
+plot(cubist_model, auto.key = list(columns = 4, lines = TRUE))
 
+# Additional code for M5
+rule_fit <- M5Rules(formula, data = data_set_train, control = Weka_control(M = 10))
+rule_fit
 
-
+# Examining fits
 model_results <- data.frame(obs = data_set[, 4],
                             Linear_Regression = predict(linear_model, data_set[,-4]))
 plot(model_results)
+model_prediction <- predict(gbm_model, data_set[,-4])
 
+# Examine predictor contributions accross all models.
 # Make a long table for all the models and the contributions of the predictors
 y <- NULL
 var_imp_table <- NULL
@@ -340,9 +313,7 @@ var_imp_table_wide <- spread(data = var_imp_table_long, key = predictor, value =
 # Some models had term not in other model, so spread fills wide table entries with NA
 var_imp_table_wide[is.na(var_imp_table_wide)] <- 0.0
 
-
 p <- ggplot(var_imp_table_long, aes(predictor, model))
 p <- p + geom_tile(aes(fill = ranking), colour = "white")
 p <- p + scale_fill_gradient(low = "white", high = "steelblue")
 p
-

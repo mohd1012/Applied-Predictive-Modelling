1df5bc0d74e6335f048bb4b880215b90e56f7599
Matthew11235 <matthew.lay@riotinto.com>
2015-07-20 17:33:03 +1000

    deleted search grid

--- varImp_reproducible_example.R
+++ varImp_reproducible_example.R
@@ -6,24 +6,17 @@ library(party)
 data_set <- diamonds[1:1000, c(1, 5, 6, 7, 8, 9, 10)]
 formula <- price ~ carat + depth + table + x + y + z
 
-enetGrid <- expand.grid(lambda = c(0, 0.01, .1), 
-                        fraction = seq(.05, 1, length = 20))
-
 set.seed(100)
 enet_model <- train(formula, 
                     data = data_set,
                     method = "enet",
-                    tuneGrid = enetGrid,
                     trControl = trainControl(method = "cv"),
                     preProc = c("center", "scale"))
-
-cGrid <- data.frame(mincriterion = sort(c(.95, seq(.75, .99, length = 2))))
 
 set.seed(100)
 ctree_model <- train(formula, 
                      data = data_set,
                      method = "ctree",
-                     tuneGrid = cGrid,
                      trControl = trainControl(method = "cv"))
 
 varImp(enet_model)

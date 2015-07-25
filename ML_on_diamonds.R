c48f748bf0a5293ab9e7809124fef6d8a5065f67
Matthew11235 <matthew.lay@riotinto.com>
2015-07-25 19:56:29 +1000

    Added line to sort model levels by cluster numbers

--- ML_on_diamonds.R
+++ ML_on_diamonds.R
@@ -369,9 +369,8 @@ var_imp_table_wide <- var_imp_table_wide[order(var_imp_table_wide$cluster),]
 # look up model in wide, get cluster, add to long
 var_imp_table_long <- merge(var_imp_table_wide[,c("model", "cluster")], var_imp_table_long, by = "model")
 var_imp_table_long <- var_imp_table_long[order(var_imp_table_long$cluster),]
+var_imp_table_long$model <- factor(var_imp_table_long$model, levels = var_imp_table_long$model[order(var_imp_table_long$cluster,var_imp_table_long$model)], ordered = TRUE)
 
-
-reorder(model, cluster)
 p <- ggplot(var_imp_table_long, aes(x = predictor, y = model, cluster))
 p <- p + geom_tile(aes(fill = ranking), colour = "white")
 p <- p + scale_fill_gradient(low = "white", high = "steelblue")
